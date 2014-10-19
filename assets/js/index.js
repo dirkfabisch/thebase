5a47ba5df527c1ae973f122a576862ab56fee452
Dirk Fabisch <dirk@base68.com>
2014-10-19 23:05:59 +0200

    Added picture_tag for responsive images

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,21 +9,21 @@
     $(document).ready(function(){
 
         $(".post-content").fitVids();
-        
+
         // Calculates Reading Time
         $('.post-content').readingTime({
             readingTimeTarget: '.post-reading-time',
             wordCountTarget: '.post-word-count',
         });
-        
+
         // Creates Captions from Alt tags
-        $(".post-content img").each(function() {
-            // Let's put a caption if there is one
-            if($(this).attr("alt"))
-              $(this).wrap('<figure class="image"></figure>')
-              .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
-        });
-        
+        // $(".post-content img").each(function() {
+        //     // Let's put a caption if there is one
+        //     if($(this).attr("alt"))
+        //       $(this).wrap('<figure class="image"></figure>')
+        //       .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
+        // });
+
     });
 
 }(jQuery));
